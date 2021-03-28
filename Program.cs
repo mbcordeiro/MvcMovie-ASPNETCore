4e3cebc7bac7a04ea7e302d90f62af420c14a626
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-03-28 19:30:44 -0300

    Add serch

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace MvcMovie
                 {
                     SeedData.Initialize(services);
                 }
-                catch
+                catch(Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occurred seeding the DB.");
